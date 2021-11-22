e67aac4a6b72719d2d6776f9744a5ccb08ef9a4f
Yury Bialou <Pradd.by@gmail.com>
2021-11-22 14:39:30 +0300

    feat: add parse url and http method

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-console.log("Hello");
+const http = require("http");
+const url = require('url');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+const server = http.createServer((req, res) => {
+  
+  const isUrlCorrectGet = /^\/persons[/]{0,1}[\d]*$/g.test(req.url);
+  const isUrlCorrectPost = /^\/persons[/]{0,1}$/g.test(req.url);
+  const isUrlCorrectPutAndDelete = /^\/persons[/]{0,1}[\d]+$/g.test(req.url);
+
+  if (isUrlCorrectGet && req.method === 'GET') {
+    const id = +req.url.split('/').reverse()[0];
+    if (isNaN(id)) {
+      // TODO: get all
+    } else {
+      // TODO: get one
+    }
+    console.log("GET", id);
+  } else if (isUrlCorrectPost && req.method == 'POST') {
+    // TODO: add person and get it
+  } else if (isUrlCorrectPutAndDelete && req.method == 'PUT') {
+    const id = +req.url.split('/').reverse()[0];
+    // TODO: edit person :id and get it
+  } else if (isUrlCorrectPutAndDelete && req.method == 'DELETE') {
+    const id = +req.url.split('/').reverse()[0];
+    // TODO: delete person :id and get it
+  } else {
+    // TODO: get 404
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server started at localhost:${PORT} ...`);
+});
