aad569cbe5336d696e874b4b872ee727b3ae38f3
Yury Bialou <Pradd.by@gmail.com>
2021-11-25 20:14:09 +0300

    feat: add functional

--- src/index.js
+++ src/index.js
@@ -1,32 +1,49 @@
 const http = require("http");
-const url = require('url');
 require('dotenv').config();
+const { getAll, getOne, create, update, destroy } = require("./controllers");
 
 const PORT = process.env.PORT || 5000;
-const server = http.createServer((req, res) => {
-  
-  const isUrlCorrectGet = /^\/persons[/]{0,1}[\d]*$/g.test(req.url);
-  const isUrlCorrectPost = /^\/persons[/]{0,1}$/g.test(req.url);
-  const isUrlCorrectPutAndDelete = /^\/persons[/]{0,1}[\d]+$/g.test(req.url);
 
-  if (isUrlCorrectGet && req.method === 'GET') {
-    const id = +req.url.split('/').reverse()[0];
-    if (isNaN(id)) {
-      // TODO: get all
-    } else {
-      // TODO: get one
-    }
-    console.log("GET", id);
-  } else if (isUrlCorrectPost && req.method == 'POST') {
-    // TODO: add person and get it
-  } else if (isUrlCorrectPutAndDelete && req.method == 'PUT') {
-    const id = +req.url.split('/').reverse()[0];
-    // TODO: edit person :id and get it
-  } else if (isUrlCorrectPutAndDelete && req.method == 'DELETE') {
-    const id = +req.url.split('/').reverse()[0];
-    // TODO: delete person :id and get it
+const server = http.createServer(async (req, res) => {
+  const isUrlCorrectWithId = /^\/persons\/[-\w]*$/g.test(req.url);
+  const isUrlCorrectWithoutId = /^\/persons$/g.test(req.url);
+  const id = req.url.split('/').reverse()[0];
+  
+  if (isUrlCorrectWithoutId && req.method === 'GET') {
+    const data = await getAll(req, res);
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(data));
+  } else if (isUrlCorrectWithId && req.method === 'GET') {
+    const data = await getOne(req, res, id);
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(data));
+  } else if (isUrlCorrectWithoutId && req.method == 'POST') {
+    create(req, res)
+      .then(result => {
+        res.writeHead(201, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(result));
+      })
+      .catch(err => {
+        res.writeHead(err.code, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ message: `${err.message}` }));
+      });
+  } else if (isUrlCorrectWithId && req.method == 'PUT') {
+    update(req, res, id)
+    .then(result => {
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(result));
+    })
+    .catch(err => {
+      res.writeHead(err.code, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ message: `${err.message}` }));
+    });
+  } else if (isUrlCorrectWithId && req.method == 'DELETE') {
+    await destroy(req, res, id);
+    res.writeHead(204, { 'Content-Type': 'application/json' });
+    res.end();
   } else {
-    // TODO: get 404
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: `Page with url: ${req.url} not fount page` }));
   }
 });
 
